33115d81ec94a990a71981347397a05ad70fc06e
Saachi <32311828+xsaachi@users.noreply.github.com>
2023-04-22 17:11:39 +0200

    XD2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { Client, GatewayIntentBits, ActivityType, Collection} = require('discord.js');
 const { Client: NotionClient } = require('@notionhq/client');
-
 const dotenv = require('dotenv').config();
 const notion = new NotionClient({ auth: process.env.NOTION_TOKEN });
-console.log (process.env.NOTION_TOKEN);
 const fs = require('fs');
+const moment = require('moment');
 
 // New user
 const client = new Client({ 
@@ -32,12 +31,21 @@ for (const folder of fs.readdirSync('./commands')) {
 // Login event
 client.once('ready', () => {
   console.log(`Logged in as: ${client.user.tag}`);
-  client.user.setPresence({
-    activities:
-    [{
-      name: 'Ailurus SMP',
-      type: ActivityType.Playing
-    }]});
+  setInterval(() => {
+    const now = moment();
+    const targetDate = moment('2023-05-12T19:00:00+00:00');
+    const duration = moment.duration(targetDate.diff(now));
+    const days = Math.floor(duration.asDays());
+    const hours = duration.hours();
+    const minutes = duration.minutes();
+    const seconds = duration.seconds();
+    const presenceText = `Do startu serwera pozostało: ${days}d ${hours}h ${minutes}m ${seconds}s`;
+    client.user.setPresence({ activities:
+      [{
+        name: presenceText,
+        type: ActivityType.Streaming
+      }]});
+  }, 1000);
   });
 
 
@@ -55,27 +63,38 @@ client.on('interactionCreate', async interaction => {
   }
 
   if (interaction.commandName === 'whitelist') {
-    const minecraftNick = interaction.options.getString('minecraft');
-    const twitchNick = interaction.options.getString('twitch');
+    try {
+      const minecraftNick = interaction.options.getString('minecraft');
+      const twitchNick = interaction.options.getString('twitch');
+  
+      // Add the user data to the Notion database
+      const databaseId = process.env.NOTION_DATABASE;
+      const newEntry = {
+        Minecraft: {
+          title: [{ text: { content: minecraftNick } }],
+        },
+        Twitch: {
+          rich_text: [{ text: { content: twitchNick } }],
+        },
+        Date: {
+          date: {
+            start: new Date().toISOString(),
+          },
+        },
 
-    // Add the user data to the Notion database
-    const databaseId = process.env.NOTION_DATABASE;
-    const newEntry = {
-      Minecraft: {
-        title: [{ text: { content: minecraftNick } }],
-      },
-      Twitch: {
-        rich_text: [{ text: { content: twitchNick } }],
-      },
-    };
-    await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: newEntry,
-    });
-
-    await interaction.reply({
-      content: `Nick ${minecraftNick} (${twitchNick}) dodany do bazy!`, ephemeral: true
-    });
+      };
+      await notion.pages.create({
+        parent: { database_id: databaseId },
+        properties: newEntry,
+      });
+  
+      await interaction.reply({
+        content: `Nick ${minecraftNick} (${twitchNick}) dodany do bazy!`, ephemeral: true
+      });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'Wystąpił błąd...', ephemeral: true });
+    }
   }
 
   
